aa4de4e70e09ac89a5168768fcb161a523d11aa5
Nathan <74594702+NSmith777@users.noreply.github.com>
2021-12-17 09:56:01 +0000

    Fix warnings in model conversion tool

--- tools/Wavefront2BinaryModel/Wavefront2BinaryModel.cpp
+++ tools/Wavefront2BinaryModel/Wavefront2BinaryModel.cpp
@@ -14,79 +14,95 @@ typedef struct {
 
 int main(int argc, char **argv)
 {
-    vector<UINT> vertexIndices, uvIndices, normalIndices;
+    vector<UINT> vertex_indices, uv_indices, normal_indices;
     vector<XMFLOAT3> temp_vertices;
     vector<XMFLOAT2> temp_uvs;
     vector<XMFLOAT3> temp_normals;
 
+    // Open Wavefront OBJ file for reading
     FILE* file = fopen(argv[1], "r");
     if (!file) {
         printf("Failed to open wavefront model file!\n");
         return -1;
     }
 
+    // Process OBJ file line-by-line and read the vertex attributes
     while(1) {
-        char lineHeader[128];
+        char line_header[128] = { '\0' };
 
         // read the first word of the line
-        int res = fscanf(file, "%s", lineHeader);
+        int res = fscanf(file, "%s", line_header);
         if (res == EOF)
             break;
-    
-        if (strcmp(lineHeader, "v") == 0) {
+        
+        if (strcmp(line_header, "v") == 0) {
             XMFLOAT3 vertex;
-            fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+
+            int read_vertex = fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+            assert(read_vertex == 3);
+
             temp_vertices.push_back(vertex);
         }
-        else if (strcmp(lineHeader, "vt") == 0) {
+        else if (strcmp(line_header, "vt") == 0) {
             XMFLOAT2 uv;
-            fscanf(file, "%f %f\n", &uv.x, &uv.y);
+
+            int read_uv = fscanf(file, "%f %f\n", &uv.x, &uv.y);
+            assert(read_uv == 2);
+
             temp_uvs.push_back(uv);
         }
-        else if (strcmp(lineHeader, "vn") == 0) {
+        else if (strcmp(line_header, "vn") == 0) {
             XMFLOAT3 normal;
-            fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+
+            int read_normal = fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+            assert(read_normal == 3);
+
             temp_normals.push_back(normal);
         }
-        else if (strcmp(lineHeader, "f") == 0) {
-            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+        else if (strcmp(line_header, "f") == 0) {
+            unsigned int vertex_index[3], uv_index[3], normal_index[3];
 
-            int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n",
-                &vertexIndex[0], &uvIndex[0], &normalIndex[0],
-                &vertexIndex[1], &uvIndex[1], &normalIndex[1],
-                &vertexIndex[2], &uvIndex[2], &normalIndex[2]
+            int read_face = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n",
+                &vertex_index[0], &uv_index[0], &normal_index[0],
+                &vertex_index[1], &uv_index[1], &normal_index[1],
+                &vertex_index[2], &uv_index[2], &normal_index[2]
                 );
             
-            vertexIndices.push_back(vertexIndex[0]);
-            vertexIndices.push_back(vertexIndex[1]);
-            vertexIndices.push_back(vertexIndex[2]);
-            uvIndices.push_back(uvIndex[0]);
-            uvIndices.push_back(uvIndex[1]);
-            uvIndices.push_back(uvIndex[2]);
-            normalIndices.push_back(normalIndex[0]);
-            normalIndices.push_back(normalIndex[1]);
-            normalIndices.push_back(normalIndex[2]);
+            assert(read_face == 9);
+
+            vertex_indices.push_back(vertex_index[0]);
+            vertex_indices.push_back(vertex_index[1]);
+            vertex_indices.push_back(vertex_index[2]);
+            uv_indices.push_back(uv_index[0]);
+            uv_indices.push_back(uv_index[1]);
+            uv_indices.push_back(uv_index[2]);
+            normal_indices.push_back(normal_index[0]);
+            normal_indices.push_back(normal_index[1]);
+            normal_indices.push_back(normal_index[2]);
         }
     }
 
+    // We're done reading the input file
     fclose(file);
 
-    // Write the binary model file
-
+    // Open output binary model file for writing
     FILE* mdl_file = fopen(argv[2], "wb");
 
-    mdl_header mdlHdr;
-    mdlHdr.magic        = 0x004C444D; // 'M' 'D' 'L' '00'
-    mdlHdr.verts_count  = vertexIndices.size();
+    // Write MDL header
+    mdl_header mdl_hdr;
+    mdl_hdr.magic        = 0x004C444D; // 'M' 'D' 'L' '00'
+    mdl_hdr.verts_count  = vertex_indices.size();
 
-    fwrite(&mdlHdr, sizeof(mdlHdr), 1, mdl_file);
+    fwrite(&mdl_hdr, sizeof(mdl_hdr), 1, mdl_file);
 
-    for (unsigned int i = 0; i < mdlHdr.verts_count; i++) {
-        fwrite(&temp_vertices[vertexIndices[i] - 1], sizeof(XMFLOAT3), 1, mdl_file);
-        fwrite(&temp_uvs[uvIndices[i] - 1], sizeof(XMFLOAT2), 1, mdl_file);
-        fwrite(&temp_normals[normalIndices[i] - 1], sizeof(XMFLOAT3), 1, mdl_file);
+    // Write MDL vertex data
+    for (unsigned int i = 0; i < mdl_hdr.verts_count; i++) {
+        fwrite(&temp_vertices[vertex_indices[i] - 1], sizeof(XMFLOAT3), 1, mdl_file);
+        fwrite(&temp_uvs[uv_indices[i] - 1], sizeof(XMFLOAT2), 1, mdl_file);
+        fwrite(&temp_normals[normal_indices[i] - 1], sizeof(XMFLOAT3), 1, mdl_file);
     }
 
+    // We're done writing the output file
     fclose(mdl_file);
 
     return 0;
